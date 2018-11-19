71ef349af41458be6496690f5418bca99d026ee6
Sjoerd20 <sjoerd.terpstra@outlook.com>
2018-11-19 17:53:23 +0100

    Added onClick handler

--- app/src/main/java/com/example/sjoerd/madlibs/MainActivity.java
+++ app/src/main/java/com/example/sjoerd/madlibs/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.sjoerd.madlibs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playButton = (Button) findViewById(R.id.buttonPlay);
+        playButton.setOnClickListener(new OnPlayButtonClickListener());
+    }
+
+    private class OnPlayButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            // TODO go to next activity with intent
+        }
+    }
+
+    // starts a MadLib game if the play button is clicked
+    public void playButtonClicked(android.view.View sender) {
+        Button b = (Button) sender;
+        b.setText("Clicked!");
     }
 }
