b3772c1d5594576305eac6556bb38969c2bcf210
Sjoerd20 <sjoerd.terpstra@outlook.com>
2018-11-20 23:14:38 +0100

    Implemented fill

--- app/src/main/java/com/example/sjoerd/madlibs/ChooseActivity.java
+++ app/src/main/java/com/example/sjoerd/madlibs/ChooseActivity.java
@@ -1,7 +1,10 @@
 package com.example.sjoerd.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.io.InputStream;
 
@@ -12,17 +15,45 @@ public class ChooseActivity extends AppCompatActivity {
                                 R.raw.madlib2_university, R.raw.madlib3_clothes,
                                 R.raw.madlib4_dance};
 
+    private String storyNames[] = {"madlib0_simple", "madlib1_tarzan", "madlib2_university",
+                                "madlib3_clothes", "madlib4_dance"};
+
+    private int buttons[] = {R.id.button0, R.id.button1, R.id.button2, R.id.button3,
+                                R.id.button4};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+
+        // set text and OnClickListener for every button
+        for (int i = 0; i < storyFiles.length; i++) {
+            Button button = findViewById(buttons[i]);
+            button.setText(storyNames[i]);
+            button.setOnClickListener(new OnStoryClickListener(buttons[i]));
+        }
     }
 
-    // TODO implement OnStoryClickListener
-    // class OnStoryClickListener implements ... {
-        // TODO index = get index
-        // InputStream is = getResources().openRawResource(storyFiles[index]);
+    private class OnStoryClickListener implements View.OnClickListener {
+
+        private int storyId;
+
+        private OnStoryClickListener(int button) {
+            this.storyId = storyFiles[button];
+        }
+
+        @Override
+        public void onClick(View view) {
 
 
+            // construct the selected story
+            InputStream inputStream = getResources().openRawResource(storyId);
+            Story story = new Story(inputStream);
+
+            // pass story to new activity called FillActivity
+            Intent intent = new Intent(ChooseActivity.this, FillActivity.class);
+            intent.putExtra("chosen_story", story);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/sjoerd/madlibs/FillActivity.java
+++ app/src/main/java/com/example/sjoerd/madlibs/FillActivity.java
@@ -1,13 +1,27 @@
 package com.example.sjoerd.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FillActivity extends AppCompatActivity {
+
+    private Story retrievedStory;
+    private int placeholderRemainingCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill);
+
+        // get intent from ChooseActivity
+        Intent intent = getIntent();
+        retrievedStory = (Story) intent.getSerializableExtra("chosen_story");
+
+        // show how many placeholders are left to be filled in
+        placeholderRemainingCount = retrievedStory.getPlaceholderRemainingCount();
+        TextView amountFilled = findViewById(R.id.textViewAmountFilled);
+        amountFilled.setText("Placeholders left: " + placeholderRemainingCount);
     }
 }
