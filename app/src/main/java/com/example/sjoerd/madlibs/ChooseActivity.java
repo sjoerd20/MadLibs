7dcf3fe4938ff4c07a0a9ea33ea6d17cabec4ea3
Sjoerd20 <sjoerd.terpstra@outlook.com>
2018-12-16 20:57:55 +0100

    Changed to list adapter

--- app/src/main/java/com/example/sjoerd/madlibs/ChooseActivity.java
+++ app/src/main/java/com/example/sjoerd/madlibs/ChooseActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import java.io.InputStream;
 
@@ -15,38 +18,27 @@ public class ChooseActivity extends AppCompatActivity {
                                 R.raw.madlib2_university, R.raw.madlib3_clothes,
                                 R.raw.madlib4_dance};
 
-    private String storyNames[] = {"madlib0_simple", "madlib1_tarzan", "madlib2_university",
-                                "madlib3_clothes", "madlib4_dance"};
-
-    private int buttons[] = {R.id.button0, R.id.button1, R.id.button2, R.id.button3,
-                                R.id.button4};
+    private String storyNames[] = {"Simple", "Tarzan", "University", "Clothes", "Dance"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
 
-        // set text and OnClickListener for every button
-        for (int i = 0; i < storyFiles.length; i++) {
-            Button button = findViewById(buttons[i]);
-            button.setText(storyNames[i]);
-            button.setOnClickListener(new OnStoryClickListener());
-        }
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        storyNames);
+        ListView list = findViewById(R.id._storyList);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new OnStoryClickListener());
     }
 
-    private class OnStoryClickListener implements View.OnClickListener {
-
+    private class OnStoryClickListener implements AdapterView.OnItemClickListener {
         @Override
-        public void onClick(View view) {
-
-            int storyId = 0;
-            int buttonId = view.getId();
-            for (int i = 0; i < buttons.length; i++) {
-                if (buttonId == buttons[i]) {
-                    storyId = storyFiles[i];
-                }
-            }
-            // construct the selected story
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            int storyId = storyFiles[i];
             InputStream inputStream = getResources().openRawResource(storyId);
             Story story = new Story(inputStream);
 
